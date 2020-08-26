a81ac5b7d0d58a5f163971d0330307e6ff20c035
LizaMarie <lizamariejimenez@gmail.com>
2020-08-26 17:28:13 -0400

    CrudRepository confusing T thing

--- Revassess/src/main/java/com/revature/dao/CrudRepository.java
+++ Revassess/src/main/java/com/revature/dao/CrudRepository.java
@@ -1,5 +1,18 @@
 package com.revature.dao;
 
-public interface CrudRepository {
+import java.util.Set;
 
+public interface CrudRepository<T> {
+
+	public void Save(T t);
+	public Set<T> finaAll();
+	
+	public void findById(int id);
+	
+	public boolean update(T t);
+	
+	public boolean deleteById(int id);
+	
+	
+	
 }
