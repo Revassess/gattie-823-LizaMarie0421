a70f5ea67534c701edb133448e023d7d33ea392a
LizaMarie <lizamariejimenez@gmail.com>
2020-08-26 15:22:48 -0400

    3rd test good

--- Revassess/src/main/java/com/revature/tier1/SumOverArray.java
+++ Revassess/src/main/java/com/revature/tier1/SumOverArray.java
@@ -3,12 +3,16 @@ package com.revature.tier1;
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
-		int sum=0;
-		for (int i =0; i <arr.length; i ++) {
-			//System.out.println("arr i : "+ arr[i]);
-			sum = sum+ arr[i];
-			//System.out.println("sum:" +sum);
-		}
-		return sum;
+		if (arr!=null) {
+			int sum=0;
+			for (int i =0; i <arr.length; i ++) {
+				System.out.println("arr i : "+ arr[i]);
+				sum = sum+ arr[i];
+				System.out.println("sum:" +sum);
+			}
+			
+			return sum;
+			}
+		return 0;
 	}
 }
