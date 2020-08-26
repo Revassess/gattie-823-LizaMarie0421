443f8eb87e6627279b80426ed3faf68ec21a4376
LizaMarie <lizamariejimenez@gmail.com>
2020-08-26 15:06:06 -0400

    2 tests first passed and added email

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -1,8 +1,30 @@
 package com.revature.tier1;
 
+import java.util.ArrayList;
+
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
+		int power = 0;
+		int sum =0;
+		
+		ArrayList<Integer> array = new ArrayList<Integer>();
+		do{
+		    array.add((int) (num % 10));
+		    num /= 10;
+		} while  (num > 0);
+		
+		for(int i =0; i <array.size();i++) {
+			power = (int) Math.pow(array.get(i), array.size());
+			sum= sum+ power;
+			System.out.println(sum);
+		}
+		Long l= new Long(sum);
+		if (num==l) {
+		    System.out.println("true");
+			return true;
+		}
+		System.out.println("false");
 		return false;
 	}
 }
--- Revassess/src/main/java/com/revature/tier1/SumOverArray.java
+++ Revassess/src/main/java/com/revature/tier1/SumOverArray.java
@@ -3,6 +3,12 @@ package com.revature.tier1;
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
-		return 0;
+		int sum=0;
+		for (int i =0; i <arr.length; i ++) {
+			System.out.println("arr i : "+ arr[i]);
+			sum = sum+ arr[i];
+			System.out.println("sum:" +sum);
+		}
+		return sum;
 	}
 }
